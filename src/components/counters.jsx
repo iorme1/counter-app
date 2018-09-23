e6ed88cf2d30bbb075f16ae044ff08390aa93e44
Isaac Orme <isorme1@gmail.com>
2018-09-22 20:31:54 -0700

    adds delete functionality

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -11,11 +11,21 @@ class Counters extends Component {
     ]
   };
 
+  handleDelete = () => {
+    console.log('Event Handler Called')
+  }
+
   render() {
     return (
       <div>
-        {this.state.counters.map(counter =>
-           <Counter key={counter.id} value={counter.value} />)}
+        {this.state.counters.map(counter => (
+          <Counter
+          key={counter.id}
+          onDelete={this.handleDelete}
+          value={counter.value}
+          id={counter.id}
+          />
+        ))}
       </div>
     );
   }
