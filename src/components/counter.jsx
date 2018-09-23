3998a346f352dffee6a131eb69c927f4bef78740
Isaac Orme <isorme1@gmail.com>
2018-09-23 13:37:53 -0700

    Adds lifecycle methods and console logs to demonstrate the flow of the components

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,7 +2,17 @@ import React,  { Component } from 'react';
 
 class Counter extends Component {
 
+  componentDidUpdate(prevProps, prevState) {
+    console.log("prevProps", prevProps)
+    console.log("prevState", prevState)
+  }
+
+  componentWillUnmount() {
+    console.log("Counter - Unmount")
+  }
+
   render() {
+    console.log('Counter - Rendered')
     return (
       <div>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
