9039002162fc339f12f52b5ee96905434c4ed8cb
Isaac Orme <isorme1@gmail.com>
2018-09-23 12:04:59 -0700

    removes local state from counter and makes it a controlled component. Adds increment handlers and navbar

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -11,19 +11,41 @@ class Counters extends Component {
     ]
   };
 
-  handleDelete = () => {
-    console.log('Event Handler Called')
+  handleDelete = (counterId) => {
+    const counters = this.state.counters.filter(c => c.id !== counterId )
+    this.setState({ counters: counters })
+  };
+
+  handleReset = () => {
+    const counters = this.state.counters.map(c => {
+      c.value = 0;
+      return c;
+    });
+    this.setState({ counters: counters })
+  };
+
+  handleIncrement = (counter) => {
+    const counters = [...this.state.counters];
+    const index = counters.indexOf(counter);
+  
+    counters[index].value++;
+    this.setState({ counters: counters })
   }
 
   render() {
     return (
       <div>
+        <button
+          onClick={this.handleReset}
+          className="btn btn-primary btn-sm m-2">
+          Reset
+        </button>
         {this.state.counters.map(counter => (
           <Counter
           key={counter.id}
           onDelete={this.handleDelete}
-          value={counter.value}
-          id={counter.id}
+          onIncrement={this.handleIncrement}
+          counter={counter}
           />
         ))}
       </div>
